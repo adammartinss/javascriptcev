45c195bb9f27613fc186d20fd0208678a7771ab6
adammartinss <103488271+adammartinss@users.noreply.github.com>
2022-04-21 21:38:07 -0300

    js

---
+++ aula12/ex013.js
@@ -0,0 +1,27 @@
+var agr = new Date()
+var diaSem = agr.getDay()
+/*Os dias são armazenados em um vetor domingo - 0 ...*/
+//console.log(`${diaSem}`)
+switch (diaSem) {
+    case 0:
+        console.log('Hoje é domingo');
+        break;
+    case 1:
+        console.log('Hoje é domingo');
+        break;
+    case 2:
+        console.log('Hoje é domingo');
+        break;
+    case 3:
+        console.log('Hoje é domingo');
+        break;
+    case 4:
+        console.log('Hoje é domingo');
+        break;
+    case 5:
+        console.log('Hoje é domingo');
+        break;
+    case 6:
+        console.log('Hoje é domingo');
+        break;
+}
