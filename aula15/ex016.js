0d7ce18f7e1105de9261fce323dcabc0b8cf295f
Adam Martins Santos <103488271+adammartinss@users.noreply.github.com>
2022-04-22 13:57:18 -0300

    funcoes

--- aula15/ex016.js
+++ aula15/ex016.js
@@ -3,6 +3,12 @@ num[5] = 6 //coloca um valor na posição manualmente
 num.push(7)//empurra diretamente o valor para a ultima posição do array automaticamente
 
 console.log(`esse é o vetor ${num}`)
-console.log(`há ${num.length} elementos`)
+console.log(`há ${num.length} posiçoes`)
 console.log(num[0])
 //num.sort() = ordena os elementos do vetor
+let pos = num.indexOf(4)//posição do valor 3
+if (pos == -1){
+    console.log('O valor não foi encontrado')
+} else{
+    console.log(`O valor está na posição ${pos}`);
+}
---
+++ aula16/fun02.js
@@ -0,0 +1,4 @@
+function soma (n1=0, n2=0){//parametros opcionais para se ficar sem valor nao ficar undefined
+    return n1+n2
+}
+console.log(soma(2, 8))
