d79fcd4c9eced375f37d7bb0ee1265d16bd9308a
adammartinss <103488271+adammartinss@users.noreply.github.com>
2022-04-29 00:17:09 -0300

    adada

---
+++ utilizandothis/thistest.js
@@ -0,0 +1,24 @@
+let pessoa = {
+    nome: "adam",
+    idade: 22,
+    falar: function() {
+        console.log("ola, mundo");
+    },
+    dizerNome:function(){
+        console.log(`Meu nome é ${this.nome}`)
+    },
+    
+    aniversario:function(){
+           this.idade += 1 
+       }
+}
+function showName(){
+    console.log()
+}
+pessoa.aniversario();
+pessoa.aniversario();
+console.log(pessoa.idade)
+pessoa.aniversario();
+console.log(pessoa.idade)
+
+pessoa.dizerNome();
