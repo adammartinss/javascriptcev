c5395bee212d39c2183fc6346b36601767b8a6b5
adammartinss <103488271+adammartinss@users.noreply.github.com>
2022-05-02 21:58:35 -0300

    s

--- utilizandothis/thistest.js
+++ utilizandothis/thistest.js
@@ -1,24 +1,54 @@
-let pessoa = {
-    nome: "adam",
-    idade: 22,
-    falar: function() {
-        console.log("ola, mundo");
-    },
-    dizerNome:function(){
-        console.log(`Meu nome é ${this.nome}`)
-    },
-    
-    aniversario:function(){
-           this.idade += 1 
-       }
-}
-function showName(){
-    console.log()
-}
-pessoa.aniversario();
-pessoa.aniversario();
-console.log(pessoa.idade)
-pessoa.aniversario();
-console.log(pessoa.idade)
+var dx;
+var dy;
+var px;
+var py;
+var obj;
+var vel;
+var tmp;
 
-pessoa.dizerNome();
+function start(){
+    dx = 0;
+    dy = 0
+    px = 0;
+    py = 0;
+    vel = 10;
+    obj = document.getElementById("div1");
+    document.addEventListener("keydown",teclaDw)
+    document.addEventListener("keyup", teclaUp)
+    tmp = setInterval(enterFrame,20)
+    
+}
+
+function teclaDw(){
+    var tecla = keyCode;
+    if(tecla == 37){
+        dx = -1;
+    }else if(tecla == 38){
+        dy=-1
+    }else if(tecla == 39){
+        dx=1
+    }else if(tecla == 40){
+        dy=1
+    }
+}
+function teclaUp(){
+     if(tecla == 37){
+        dx = 0;
+    }else if(tecla == 38){
+        dy=0
+    }else if(tecla == 39){
+        dx=0
+    }else if(tecla == 40){
+        dy=0
+    }
+}
+
+function enterFrame(){
+    px += dx*vel;
+    py += dy*vel;
+    obj.style.left = px+"px"
+    obj.style.top = py+"px"
+    
+}
+window.addEventListener("load", start)
+
